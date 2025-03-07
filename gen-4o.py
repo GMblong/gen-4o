aee826bdcd7e3b5421b8f9d3f113a2687d8be048
GMblong <andiarifrahmatullah@gmail.com>
2025-03-07 11:43:18 +0700

    proxy

--- gen-4o.py
+++ gen-4o.py
@@ -566,6 +566,7 @@ def init_driver(twofa_code="", account_type="Demo", username_input="", password_
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
         
         try:
             # Asumsi chromium-driver sudah ada di PATH
